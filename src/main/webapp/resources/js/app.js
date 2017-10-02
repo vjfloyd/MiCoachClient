cceed7c0c5a9391fc5cdd4d66af3741ccd2c6a99
Victor Rojas <vjrojasb@gmail.com>
2017-10-02 00:12:10 -0500

    add

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,15 +1,3 @@
-var app = angular.module('app', ['ngRoute','ngResource']);
-app.config(function($routeProvider){
-    $routeProvider
-        .when('/users',{
-            templateUrl: '/views/users.html',
-            controller: 'usersController'
-        })
-        .when('/roles',{
-            templateUrl: '/views/roles.html',
-            controller: 'rolesController'
-        })
-        .otherwise(
-            { redirectTo: '/'}
-        );
-});
+'use strict';
+
+var App = angular.module('myApp',[]);
